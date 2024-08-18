4c330e0f5017ca256bf3c8b9cb8d72ef561b4003
efraimff <emfs6@aluno.ifal.edu.br>
2024-08-18 00:20:25 -0300

    feat: implement complete user CRUD functionality (#1)
    - Add user registration endpoint
    - Implement validation for user data
    - Store users in the database with hashed passwords
    - Add endpoints for updating and deleting users
    - Fetch user details by ID or username

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from "./modules/user/user.module";
+import { PrismaService } from "./prisma.service";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
---
+++ src/modules/user/dto/updateUser.dto.ts
@@ -0,0 +1,6 @@
+export interface UpdateUserDto{
+  username?: string;
+  email?: string;
+  password?: string;
+  profile_visibility?: number;
+}
