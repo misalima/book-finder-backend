b63f3434ce10cd1aa2c19a7e33e6c27edbd980ed
Misael Lima <misael.alisson14@gmail.com>
2024-08-30 16:56:16 -0300

    modify main.ts to enable CORS policy

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
- 
+  app.enableCors();
   await app.listen(3333);
 }
 bootstrap();
